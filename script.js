7112d61214d2fac041cddc730aad18e216841ba5
mikolasaputra <123238409+mikolasyaputra@users.noreply.github.com>
2024-07-19 15:48:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 // ================================ typed js ========================*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'Backend Developer', 'ui/ux Designer'],
+    strings: ['Frontend Developer', 'ui/ux Designer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
